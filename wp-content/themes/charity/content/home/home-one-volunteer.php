52f9feba95ddf7b09fad006f526f99b795410737
kautzar <kalikautsar@yahoo.com>
2015-10-09 13:53:35 +0700

    yeay

--- wp-content/themes/charity/content/home/home-one-volunteer.php
+++ wp-content/themes/charity/content/home/home-one-volunteer.php
@@ -14,15 +14,15 @@ $content = vp_metabox('volunteer.volunteer-content');
 $url = wp_get_attachment_image_src( get_post_thumbnail_id($pageid), array( 1143,479 ) );
  ?>
 <!--section class="parallax-section parallax" style="background-image: url('<?php echo esc_url($image);?>')"-->
-<section class="parallax-section parallax" style="background-image: url(http://localhost/drpp4/wp-content/uploads/2015/08/Foto-Ibuk-dan-dua-anaknya.jpg)">
+<section class="parallax-section parallax" style="background-image: url(http://localhost/drpp4/wp-content/uploads/2015/10/19169785441_93f9534163_c.jpg)">
 	<div class="container">
 		<div class="row">
 			<div class="col-xs-12 col-sm-7 col-md-5">
 				<!--h2><?php print($title); ?></h2>
 				<p><?php print($content); ?></p-->
-				<h2 style="color:black">Join Membership</h2>
-				<p style="color:black">Become Our Member Now</p>
-				<a href="http://localhost/drpp" style="border-color:black; color:black" class="btn btn-default btn-lg join-today">JOIN TODAY</a>
+				<h2 style="color:black">Menjadi Anggota The Learning Center</h2>
+				<p style="color:black">Bergabunglah dengan kami sekarang</p>
+				<a href="http://localhost/drpp" style="border-color:black; color:black" class="btn btn-default btn-lg join-today">DAFTAR</a>
 			</div>
 		</div>
 	</div>
